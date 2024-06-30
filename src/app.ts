0a4e7877b58466eff834236771d999a8e6b22acd
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-01 00:03:24 +0100

    patch: fix ip behind proxy

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,59 @@
-import type { Response, Request } from 'express-serve-static-core';
-import express from 'express';
-import cors from 'cors';
-import { getIPDetails, getTemperature } from './services';
+import type { Response, Request } from "express-serve-static-core";
+import express from "express";
+import cors from "cors";
+import { getIPDetails, getTemperature } from "./services";
 const app = express();
+
+app.set("trust proxy", true);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 
-app.get('/api/hello', async (req: Request, res: Response) => {
+app.get("/api/hello", async (req: Request, res: Response) => {
   try {
-    const { visitorName } = <{ visitorName: string }>req.query;
+    const { visitor_name } = <{ visitor_name: string }>req.query;
 
-    if (!visitorName) return res.status(400).json({ error: true, message: 'Visitor name is required is request query' });
+    if (!visitor_name)
+      return res.status(400).json({
+        error: true,
+        message: "Visitor name is required is request query",
+      });
 
-    const ip = req.ip as string;
+    const ip = (req.headers["x-forwarded-for"] ||
+      req.socket.remoteAddress) as string;
 
-    if (!ip || typeof ip === 'undefined') return res.status(500).json({ error: true, message: 'Could not get user IP Data, please try again later' });
+    if (!ip || typeof ip === "undefined")
+      return res.status(500).json({
+        error: true,
+        message: "Could not get user IP Data, please try again later",
+      });
 
     const { city, lat, lon } = await getIPDetails(ip);
     const { temp } = await getTemperature(lon, lat);
 
-    const greeting = `Hello, ${visitorName}, the temperature is ${temp} degree Celcius in ${city}`;
+    const greeting = `Hello, ${visitor_name}, the temperature is ${Math.round(
+      temp
+    )} degree Celcius in ${city}`;
 
     res.status(200).json({ client_ip: ip, location: city, greeting });
   } catch (error) {
-    res.status(500).json({ error: true, message: 'Something went wrong, please try again later', severMessage: (error as Error).message });
+    res.status(500).json({
+      error: true,
+      message: "Something went wrong, please try again later",
+    });
   }
 });
 
+app.all("*", (req: Request, res: Response) =>
+  res.status(400).json({
+    error: true,
+    message:
+      "Invalid endpoint or method used, please check and try again later",
+  })
+);
 
+const PORT = (process.env.PORT as string) || "9000";
 
-app.all('*', (req: Request, res: Response) => res.status(400).json({ error: true, message: 'Invalid endpoint or method used, please check and try again later' }));
-
-const PORT = process.env.PORT as string || '9000';
-
-app.listen(PORT, () => console.log(`Server running on ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
